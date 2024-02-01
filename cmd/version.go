8fbb4b77d3fc6ed4c3777bd541e2b70406b64726
Guionardo Furlan - Levva <guionardo.furlan.ext@jeitto.com.br>
2024-02-01 15:47:30 -0300

    Update action

--- cmd/version.go
+++ cmd/version.go
@@ -1,7 +1,3 @@
-/*
-Copyright © 2022 NAME HERE <EMAIL ADDRESS>
-
-*/
 package cmd
 
 import (
@@ -9,25 +5,12 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// versionCmd represents the version command
-var versionCmd = &cobra.Command{
-	Use:   "version",
-	Short: "Application version",
-	Run: func(cmd *cobra.Command, args []string) {
-		cmd.Printf("%s v%s\n", app.ToolName, app.Version)
-	},
-}
-
 func init() {
-	rootCmd.AddCommand(versionCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// versionCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// versionCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.AddCommand(&cobra.Command{
+		Use:   "version",
+		Short: "Application version",
+		Run: func(cmd *cobra.Command, args []string) {
+			cmd.Printf("%s v%s\n", app.ToolName, app.Version)
+		},
+	})
 }
