1901304dc6bedb512506d6c12b2b72f5917f8609
Guionardo Furlan - Levva <guionardo.furlan.ext@jeitto.com.br>
2024-02-02 11:28:41 -0300

    Updated version to 0.2.1

--- app/metadata.go
+++ app/metadata.go
@@ -1,7 +1,7 @@
 package app
 
 const ToolName = "gs-dev"
-const Version = "0.2.0"
+const Version = "0.2.1"
 const Description = `Guiosoft Development Assistant is a tool for helping
 the developer in your tasks.`
 const ShortDescription = "Guiosoft Development Assistant"
