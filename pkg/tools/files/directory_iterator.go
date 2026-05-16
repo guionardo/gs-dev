7a64432fad6aa846889bf1d2daf789cd5938fdee
Guionardo Furlan <guionardo@gmail.com>
2026-05-16 11:01:36 -0300

    refactor: optimize read directory

--- pkg/tools/files/directory_iterator.go
+++ pkg/tools/files/directory_iterator.go
@@ -15,52 +15,8 @@ var (
 
 // ReadDirectory reads the directory at the given path and yields the paths of all subdirectories up to the specified max depth.
 // It returns a generator with only the directory paths limited to maxDept
-func ReadDirectory(basePath string, maxDept int) iter.Seq[string] {
-	return readDirectory2(basePath, 0, maxDept)
-}
-
-func readDirectory(basePath string, level, maxDepth int) iter.Seq[string] {
+func ReadDirectory(basePath string, maxDepth int) iter.Seq[string] {
 	return func(yield func(string) bool) {
-		if level >= maxDepth {
-			return
-		}
-		// Check if the basepath has one of ignoreDirsSet to avoid sub reading
-		if directoryHasIgnoredSubFolder(basePath) {
-			return
-		}
-
-		entries, err := os.ReadDir(basePath)
-		if err != nil {
-			return
-		}
-
-		for _, entry := range entries {
-			if !entry.IsDir() {
-				continue
-			}
-
-			dirPath := path.Join(basePath, entry.Name())
-			if !yield(dirPath) {
-				return
-			}
-
-			if level < maxDepth-1 {
-				for dir := range readDirectory(dirPath, level+1, maxDepth) {
-					if !yield(dir) {
-						return
-					}
-				}
-			}
-		}
-	}
-}
-
-func readDirectory2(basePath string, level, maxDepth int) iter.Seq[string] {
-	return func(yield func(string) bool) {
-		if level >= maxDepth {
-			return
-		}
-
 		type frame struct {
 			path  string
 			level int
@@ -69,7 +25,7 @@ func readDirectory2(basePath string, level, maxDepth int) iter.Seq[string] {
 		}
 
 		rootDirs := getSubDirs(basePath)
-		stack := []frame{{basePath, level, rootDirs, 0}}
+		stack := []frame{{basePath, 0, rootDirs, 0}}
 
 		for len(stack) > 0 {
 			top := &stack[len(stack)-1]
